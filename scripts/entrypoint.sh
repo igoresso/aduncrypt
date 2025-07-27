9c3c59098e544ee2dd6ce7e7331437320a6077d2
Igor Tyshchenko <33253240+igoresso@users.noreply.github.com>
2025-07-27 10:29:26 +1000

    Set correct permissions for AdGuard Home directories

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,14 +2,22 @@
 
 crond -b
 
-echo "Checking unbound configuration..."
+echo "Checking Unbound configuration..."
 unbound-checkconf /opt/unbound/unbound.conf
 if [ $? -ne 0 ]; then
   echo "Unbound configuration is invalid: $?"
   exit 1
 fi
 
-# Start DNSCrypt-Proxy
+if [ ! -f /var/lib/unbound/root.key ]; then
+  echo "Bootstrapping the root trust anchor for DNSSEC validation..."
+  unbound-anchor -a /var/lib/unbound/root.key
+fi
+
+echo "Setting correct permissions for AdGuard Home directories..."
+chmod 700 /opt/adguardhome/work
+chmod 700 /opt/adguardhome/conf
+
 echo "Starting DNSCrypt-Proxy..."
 dnscrypt-proxy -config /opt/dnscrypt/dnscrypt-proxy.toml &
 if [ $? -ne 0 ]; then
@@ -17,13 +25,7 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-sleep 5  # Wait for DNSCrypt-Proxy to initialize
-
-# Start Unbound
-if [ ! -f /var/lib/unbound/root.key ]; then
-  echo "Bootstrapping the root trust anchor for DNSSEC validation..."
-  unbound-anchor -a /var/lib/unbound/root.key
-fi
+sleep 5
 
 echo "Starting Unbound DNS resolver..."
 unbound -d -c /opt/unbound/unbound.conf &
@@ -32,6 +34,5 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# Start AdGuard Home
 echo "Starting AdGuard Home..."
 exec /opt/adguardhome/AdGuardHome -c /opt/adguardhome/conf/AdGuardHome.yaml -w /opt/adguardhome/work --no-check-update
